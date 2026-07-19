d80db058194a9c165056e7ab0d4daeb6f0cf810e
sumit-pujari <sumitpujari1012@gmail.com>
2026-07-19 18:55:58 +0530

    5DefaultConstructor.cpp

---
+++ 5DefaultConstructor.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+class customer{
+    string name;
+    string acc_no;
+
+    public:
+    customer(){
+        cout<<"constructor is called"<<endl;
+    }
+};
+int main(){
+    customer c1;
+}

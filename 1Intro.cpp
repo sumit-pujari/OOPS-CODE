529fe5da3474b6516b06c77f99b78a714c8a68d4
sumit-pujari <sumitpujari1012@gmail.com>
2026-07-19 18:37:51 +0530

    Add 1Intro.cpp

---
+++ 1Intro.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+class student{
+    public:
+    string name;
+    int age,rollno;
+    string grade;
+};
+
+int main(){
+    student s1;
+    s1.name="Sumit";
+    s1.age=21;
+    s1.rollno=12;
+    s1.grade="A+";
+    
+    cout<<s1.name<<endl;
+    
+}
